64777ff58f2a11088fd7da202898cc24d0a806c1
uflowie <bananasawsm@gmail.com>
2025-09-10 23:49:30 +0200

    scanner part 1

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,3 +1,82 @@
+use std::{env::args, fs::read_to_string, io::stdin};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = args().collect();
+
+    if args.len() > 3 {
+        println!("Usage: jlox [script]");
+    } else if args.len() == 3 {
+        run_file(&args[0]);
+    } else {
+        run_prompt();
+    }
 }
+
+fn run_file(file_name: &str) {
+    let program = read_to_string(file_name).expect("file content to not be empty");
+    run(&program);
+}
+
+fn run_prompt() {
+    let mut line = String::new();
+
+    loop {
+        stdin()
+            .read_line(&mut line)
+            .expect("user input to not not be empty");
+        run(&line);
+        line.clear();
+    }
+}
+
+fn run(program: &str) {
+    println!("{}", program);
+}
+
+fn get_tokens(program: &str) -> Result<Vec<Token>, ScanningError> {
+    Ok(vec![])
+}
+
+enum Token {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    Identifier(String),
+    String(String),
+    Number(i64),
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    EOF,
+}
+
+enum ScanningError {}
