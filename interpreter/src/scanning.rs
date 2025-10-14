65afb7ff1c4071f366c30b5d44e6eea3d8207572
uflowie <bananasawsm@gmail.com>
2025-10-14 23:58:44 +0200

    scanning: winnow wip

--- interpreter/src/scanning.rs
+++ interpreter/src/scanning.rs
@@ -1,3 +1,22 @@
+use winnow::combinator::alt;
+use winnow::{Parser, Result};
+
+fn parse_token(input: &mut &str) -> Result<Token> {
+    alt((
+        '('.value(Token::LeftParen),
+        ')'.value(Token::RightParen),
+        '{'.value(Token::LeftBrace),
+        '}'.value(Token::RightBrace),
+        ','.value(Token::Comma),
+        '.'.value(Token::Dot),
+        '-'.value(Token::Minus),
+        '+'.value(Token::Plus),
+        ';'.value(Token::Semicolon),
+        '*'.value(Token::Star),
+    ))
+    .parse_next(input)
+}
+
 pub fn get_tokens(program: &str) -> Result<Vec<Token>, ScanningError> {
     let mut tokens = vec![];
     let mut line = 1;
@@ -139,7 +158,7 @@ pub fn get_tokens(program: &str) -> Result<Vec<Token>, ScanningError> {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
     LeftParen,
     RightParen,
